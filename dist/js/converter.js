6133cc2e84eef74aacd0df147a93c9cac45cb427
Victor Eke <evavic44@gmail.com>
2022-06-25 19:21:31 +0100

    create convert form

--- dist/js/converter.js
+++ dist/js/converter.js
@@ -1,4 +1,11 @@
 gsap.to(".content", { duration: 3, y: 50, opacity: "1", ease: "expo.out" });
+// gsap.to(".converter", {
+//   duration: 2,
+//   y: 50,
+//   opacity: "1",
+//   delay: "-1",
+//   ease: "expo.out",
+// });
 gsap.to(".logo", {
   duration: 2,
   y: 30,
