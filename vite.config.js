6c3d55c58418a257910d264a40f05026930d7ddd
Victor Eke <evavic44@gmail.com>
2024-08-02 14:55:22 +0100

    deploy nodejs api on vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import commonjs from "vite-plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), commonjs()],
+  plugins: [react(), svgr()],
 });
